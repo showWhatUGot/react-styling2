20eb5b09c69c5d0700b65986683dbd434d815eb7
Call_Ma_Name <93004055+showWhatUGot@users.noreply.github.com>
2022-08-08 02:43:45 +0900

    백틱 사용 x

--- react-styling2/src/CSSModule.js
+++ react-styling2/src/CSSModule.js
@@ -3,7 +3,7 @@ import styles from "./CSSModule.module.css";
 
 const CSSModule = () => {
   return (
-    <div className={`${styles.wrapper} ${styles.inverted}`}>
+    <div className={[styles.wrapper, styles.inverted].join(" ")}>
       하이 <sapn className="something">css module.</sapn>
     </div>
   );
