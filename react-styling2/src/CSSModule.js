6acc68685a7b27b6a652878dc9175f4e2b98bad7
Call_Ma_Name <93004055+showWhatUGot@users.noreply.github.com>
2022-08-08 02:52:17 +0900

    classnames + CSS Module

--- react-styling2/src/CSSModule.js
+++ react-styling2/src/CSSModule.js
@@ -1,9 +1,12 @@
 import React from "react";
+import classNames from "classnames";
 import styles from "./CSSModule.module.css";
+
+const cx = classNames.bind(styles);
 
 const CSSModule = () => {
   return (
-    <div className={[styles.wrapper, styles.inverted].join(" ")}>
+    <div className={cx("wrapper", "inverted")}>
       하이 <sapn className="something">css module.</sapn>
     </div>
   );
