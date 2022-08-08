e6aa7ed47a36062feeb1140e04e549102b4a3bed
Call_Ma_Name <93004055+showWhatUGot@users.noreply.github.com>
2022-08-08 19:09:15 +0900

    tagged 템플릿 리터럴 + media 함수화

--- react-styling2/src/App.js
+++ react-styling2/src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
-import CSSModule from "./CSSModule.js";
-
+import StyledComponent from "./StyledComponent";
 class App extends Component {
   render() {
     return (
       <div>
-        <CSSModule />
+        <StyledComponent />
       </div>
     );
   }
